1e10f9046f9f97cd2607dd0ba8d05890dc3f9f10
DESKTOP\kksyr <kksyrr@gmail.com>
2024-02-19 20:15:47 +0900

    Initial Commit

---
+++ InitialGridOperator.py
@@ -0,0 +1,43 @@
+import bpy
+
+
+def main(context):
+    ob = context.active_object
+    for vertex in ob.data.vertices:
+        vertex.co.z = (1-pow(vertex.co.x,4))*(1-pow(vertex.co.y,4))
+
+
+class InitialGrid(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.initialgrid_operator"
+    bl_label = "Initial Grid Operator"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        main(context)
+        return {'FINISHED'}
+
+
+def menu_func(self, context):
+    self.layout.operator(InitialGrid.bl_idname, text=InitialGrid.bl_label)
+
+
+# Register and add to the "object" menu (required to also use F3 search "Simple Object Operator" for quick access).
+def register():
+    bpy.utils.register_class(InitialGrid)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+
+
+def unregister():
+    bpy.utils.unregister_class(InitialGrid)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.object.initialgrid_operator()
---
+++ SphericalGridOperator.py
@@ -0,0 +1,50 @@
+import bpy
+from mathutils import *
+
+def main(context):
+    ob = context.active_object
+    for vertex in ob.data.vertices:
+        vertex_co = Vector((vertex.co.x,vertex.co.y,vertex.co.z))
+        vertex_co.normalize()
+        
+        #radius
+        radius_scalar = 100
+        
+        vertex.co.x = vertex_co[0]*radius_scalar
+        vertex.co.y = vertex_co[1]*radius_scalar
+        vertex.co.z = vertex_co[2]*radius_scalar
+        
+class SphericalGrid(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "object.sphericalgrid_operator"
+    bl_label = "Spherical Grid Operator"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        main(context)
+        return {'FINISHED'}
+
+
+def menu_func(self, context):
+    self.layout.operator(InitialGrid.bl_idname, text=SphericalGrid.bl_label)
+
+
+# Register and add to the "object" menu (required to also use F3 search "Simple Object Operator" for quick access).
+def register():
+    bpy.utils.register_class(SphericalGrid)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+
+
+def unregister():
+    bpy.utils.unregister_class(SphericalGrid)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.object.sphericalgrid_operator()
